d34308711abe53196d71dc526bfaa81f15411d38
nurzainpradana <pradananurzain@gmail.com>
2020-11-26 16:34:25 +0700

    create halamanutama.java

---
+++ app/src/main/java/com/zainpradana/sqlitepenjualan/ui/halamanutama/HalamanUtama.java
@@ -0,0 +1,40 @@
+package com.zainpradana.sqlitepenjualan.ui.halamanutama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.zainpradana.sqlitepenjualan.R;
+
+public class HalamanUtama extends AppCompatActivity {
+    Button buttonDataPelanggan, buttonDataBarang, buttonPenjualan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_utama);
+
+        buttonDataPelanggan = findViewById(R.id.button_data_pelanggan);
+        buttonDataBarang = findViewById(R.id.button_data_barang);
+        buttonPenjualan = findViewById(R.id.button_data_penjualan);
+
+        buttonDataPelanggan.setOnClickListener(view -> {
+            Intent goToDataPelanggan = new Intent(HalamanUtama.this, DataPelanggan.class);
+            startActivity(goToDataPelanggan);
+        });
+
+        buttonDataBarang.setOnClickListener(view -> {
+            Intent goToDataBarang = new Intent(HalamanUtama.this, DataBarang.class);
+            startActivity(goToDataBarang);
+        });
+
+        buttonPenjualan.setOnClickListener(view -> {
+            Intent goToDataPenjualan = new Intent(HalamanUtama.this, DataPenjualan.class);
+            startActivity(goToDataPenjualan);
+        });
+
+
+    }
+}
--- app/src/main/java/com/zainpradana/sqlitepenjualan/ui/login/MainActivity.java
+++ app/src/main/java/com/zainpradana/sqlitepenjualan/ui/login/MainActivity.java
@@ -1,4 +1,4 @@
-package com.zainpradana.sqlitepenjualan.view.login;
+package com.zainpradana.sqlitepenjualan.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.zainpradana.sqlitepenjualan.R;
+import com.zainpradana.sqlitepenjualan.ui.halamanutama.HalamanUtama;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
